6f1ff2a96760696b43709ac3bb01d43cc9bf6c7a
Rajat Gupta <imrajatgupta@gmail.com>
2020-07-11 17:32:46 -0400

    Product btn, img, custom styledcomponent implement

--- src/components/Product.js
+++ src/components/Product.js
@@ -31,6 +31,14 @@ export default class Product extends Component {
               )}
               </button>
           </div>
+          {/* card footer */}
+          <div className='card-footer d-flex justify-content-between'>
+                <p className='align-self-center mb-0'>{title}</p>
+                <h5 className='text-blue mb-0'>
+                  <span className='mr-1'>$</span>
+                  {price}
+                </h5>
+          </div>
         </div>
       </ProductWrapper>
     )
@@ -38,5 +46,60 @@ export default class Product extends Component {
 }
 
 const ProductWrapper = styled.div`
+  .card {
+    border-color: transparent;
+    transition: all 1s linear;
+  }
 
-`
+  .card-footer {
+    border-top: transparent;
+    background: transparent;
+    transition: all 1s linear;
+  }
+
+  &:hover {
+    .card {
+      border: 0.04em solid rgba(0,0,0,0.2);
+      box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.2);
+    }
+    .card-footer {
+      background: rgba(240, 240, 240);
+    }
+  }
+  
+  .img-container {
+    position: relative;
+    overflow: hidden;
+  }
+
+  .card-img-top {
+    transition: all 0.5s linear;
+  }
+
+  .img-container:hover .card-img-top {
+    transform: scale(1.2);
+  }
+
+  .cart-btn {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: 0.2rem 0.4rem;
+    background: var(--lightBlue);
+    border: none;
+    color: var(--mainWhite);
+    font-size: 1.4rem;
+    border-radius: 0.5rem 0 0 0;
+    transform: translate(100%, 100%);
+    transition: all 0.5s linear;
+  }
+
+  .img-container:hover .cart-btn {
+    transform: translate(0, 0);
+  }
+
+  .cart-btn:hover {
+    color: var(--mainBlue);
+    cursor: pointer;
+  }
+`;
