8d4d28240dfd9c27a3472c17b0ca6d94af29bcfa
Rajat Gupta <imrajatgupta@gmail.com>
2020-07-11 15:20:16 -0400

    Product component setup, data renders, button work

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
 import Product from './Product';
 import Title from './Title';
-import { storeProducts } from '../data';
 import { ProductConsumer } from '../context';
 
 export default class ProductList extends Component {
-  state = {
-    // storeProducts comes from data.js
-    products : storeProducts
-  };
 
   render() {
     // console.log(this.state.products);
